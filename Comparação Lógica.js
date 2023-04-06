8ae16d9448f8356cefb5b6009fb3fa6b08bb9810
João Vitor <129129458+Ultralitz-Dev@users.noreply.github.com>
2023-04-05 22:12:55 -0300

    Add files via upload

---
+++ Comparação Lógica.js
@@ -0,0 +1,34 @@
+    false || false
+    true  || false
+    false || true
+    true  || true
+
+    false && false
+    true  && false
+    false && true
+    true  && true 
+
+    false || false || true 
+    false && false && true 
+    false || true && true
+
+    (false || true) && true
+
+    !false
+    !true
+
+    !(10 < 20)
+
+    (1 < 2) || (3 > 4)
+    (1 > 2) || (3 > 4)
+
+    (10 < 20) && (20 < 30)
+    (20 < 30) && (30 > 40)
+
+    (20 < 30) && !(30 > 40)
+
+    var a = 10
+    var b = 20
+    var c = 30  
+        
+    (a < b) && (b < c)
